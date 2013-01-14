2ad9e90a6d2cd9881eeb454a1f89a82863fde490
j1anb1n <chan.jianbin@gmail.com>
2013-01-14 20:54:27 +0800

    更新config设置方式

--- grunt.js
+++ grunt.js
@@ -1,6 +1,67 @@
+var config = {
+    "lint": {
+        "files": [
+            "src/core/boot.js",
+            "src/core/deferred.js",
+            "src/core/loader.js",
+            "src/core/util.js",
+            "src/util/**/*.js",
+            "src/app/**/*.js",
+            "bin/tasks/*.js"
+        ]
+    },
+    "jshint": {
+        "options": {
+            "browser": true,
+            "scripturl": true
+        }
+    },
+    "concat": {
+        "dist/g.js": {
+            "src": [
+                "src/core/es5-safe.js",
+                "src/core/json2.js",
+                "src/core/boot.js",
+                "src/core/util.js",
+                "src/core/deferred.js",
+                "src/core/loader.js"
+            ],
+            "dest": "dist/g.js"
+        },
+        "dist/g-mobil.js": {
+            "src": [
+                "src/core/boot.js",
+                "src/core/util.js",
+                "src/core/deferred.js",
+                "src/core/loader.js"
+            ],
+            "dest": "dist/g-mobil.js"
+        },
+        "src/g.js": {
+            "src": [
+                "src/core/es5-safe.js",
+                "src/core/json2.js",
+                "src/core/boot.js",
+                "src/core/util.js",
+                "src/core/deferred.js",
+                "src/core/loader.js"
+            ],
+            "dest": "src/g.js"
+        }
+    },
+    "build-config": {
+        "files": "src/**/config.json",
+        "dest": "public/config.json"
+    },
+    "build-js": {
+        "files"  : "src/**/*.js",
+        "dest"   : "public/",
+        "exclude": ["src/core/*.js"]
+    }
+};
+
 module.exports = function(grunt) {
     grunt.task.searchDirs.push('.');
-    var config = grunt.file.readJSON('config.json');
     grunt.loadTasks('bin/tasks');
     grunt.initConfig(config);
     grunt.registerTask('default', 'lint concat build-js');
